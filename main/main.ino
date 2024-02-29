91341956390531ee6b1399f60439f87dd1dea85a
Esmaeill <liyanboy74@gmail.com>
2024-02-29 13:46:42 +0330

    test code

--- main/main.ino
+++ main/main.ino
@@ -1,25 +1,21 @@
 #include <Arduino.h>
+#include <string.h>
+#include <stdio.h>
 
 #include "radio.h"
 #include "keeloq.h"
+#include "hcs301.h"
 
 #define LED_PIN LED_BUILTIN
 
-typedef struct{
-  uint32_t counter:16;
-  uint32_t disc:10;
-  uint32_t ovr:2;
-  uint32_t btn:4;
-}encrypted_p_s;
-
-typedef struct{
-  uint32_t ser:28;
-  uint32_t btn:4;
-}fixed_p_s;
-
+char Buffer[64];
 
 uint64_v key= {0x5cec6701,0xb79fd949};
 
+struct hcsFixed hcs_fix;
+struct hcsEncrypted hcs_enc;
+
+uint32_t ciphertext,plaintext;
 
 void blink() {
   digitalWrite(LED_PIN, !digitalRead(LED_PIN));
@@ -28,7 +24,6 @@ void blink() {
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  Serial.println("\r\nStarting..");
   pinMode(LED_PIN, OUTPUT);
   radio_init(&radio);
   Serial.println("Ready");
@@ -36,14 +31,19 @@ void setup() {
 
 void loop() {
   if (radio_rx_data_is_ready(&radio)) {
+
+    
+    hcs_fix=*(struct hcsFixed*)&radio.dataF;
+
+    ciphertext=radio.dataE;
+    keeloq_decrypt(&key,&plaintext,&ciphertext,KEELOQ_NROUNDS);
+    hcs_enc=*(struct hcsEncrypted*)&plaintext;
+
+    sprintf(Buffer, "%x \r\n",hcs_fix.ser);
+    
+    Serial.print(Buffer);
+
     blink();
-    Serial.print(radio.dataF);
-    Serial.print(radio.dataE);
-    Serial.println(radio.dataVR);
-
-    // Decrypt ciphertext to plaintext
-       // keeloq_decrypt(&key,&plaintext,&ciphertext,KEELOQ_NROUNDS);
-
     radio_rx_reset(&radio);
   }
 }
