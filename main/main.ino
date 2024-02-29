55dd2f4c5880ef72ad49f00157a06ab5a9dcc2d3
Esmaeill <liyanboy74@gmail.com>
2024-02-29 11:52:54 +0330

    debug synce ok

--- main/main.ino
+++ main/main.ino
@@ -1,9 +1,9 @@
 #include <Arduino.h>
 
-#define RADIO_SIGNAL_PIN 2 //Intrrupt 0
+#define RADIO_SIGNAL_PIN 2  //Intrrupt 0
 #define LED_PIN LED_BUILTIN
 
-enum radio_s_e{
+enum radio_s_e {
   RADIO_OFF,
   RADIO_LISTEN,
   RADIO_SYNC_OK,
@@ -15,158 +15,117 @@ struct oneBit {
   uint16_t off;
 };
 
-struct pulseFilter {
-  uint16_t pulselength;
-  uint16_t err;
-};
-
 struct radioRXC {
-  uint8_t index;
-  uint8_t sp;
   radio_s_e state;
+  uint8_t sp;
+  uint8_t index;
   uint16_t pulselength;
-  const pulseFilter *filter;
-  uint32_t dataE,dataF;
+  uint32_t dataE, dataF;
   uint8_t dataVR;
 };
 
-struct pulseFilter f1 = { .pulselength = 400, .err = 100 };
 struct radioRXC radio;
 
-void radio_init(radioRXC *obj) {
+uint32_t tempt = 0, oldt = 0, dift = 0;
+volatile oneBit tempb;
+
+void radio_init(void) {
 
   pinMode(RADIO_SIGNAL_PIN, INPUT_PULLUP);
   attachInterrupt(0, RADIO_SIGNAL_PIN_handler, CHANGE);
 
-  obj->state=RADIO_LISTEN;
-  obj->filter = &f1;
+  radio.state = RADIO_LISTEN;
+  radio.sp = 0;
 }
 
-bool is_squad_pulse(radioRXC *obj,oneBit *d)
-{
-  if(abs((int)d->on-(int)d->off)<obj->filter->err)return true;
-  return false;
+void radio_check_synce_pulse(void) {
+  if (abs((int)tempb.on - (int)tempb.off) < 100)  // is squad pulse ?
+    radio.sp++;
+  else if (tempb.off > (tempb.on * 8) && tempb.off < (tempb.on * 12) && radio.sp > 10) {
+    radio.index = 0;
+    radio.pulselength = 0;
+    radio.dataE = 0;
+    radio.dataF = 0;
+    radio.dataVR = 0;
+    //radio.state = RADIO_SYNC_OK;
+    radio.sp = 0;
+
+    //Debug
+    blink();
+  } else
+    radio.sp = 0;
 }
 
-bool radio_is_valid_pulse(radioRXC *obj,oneBit *d)
-{
-  uint16_t min=d->on;
-  if(d->off<min)min=d->off;
-  if(min<((obj->filter->pulselength)-(obj->filter->err)))return false;
-  if(min>((obj->filter->pulselength)+(obj->filter->err)))return false;
-  return true;
-}
-
-void radio_check_synce_pulse(radioRXC *obj,oneBit *d)
-{
-  if(is_squad_pulse(obj,d))obj->sp++;
-  else if(d->off>(d->on*8) && d->off<(d->on*12) && obj->sp>10)
-  {
-    obj->index=0;
-    obj->pulselength=0;
-    obj->dataE=0;
-    obj->dataF=0;
-    obj->dataVR=0;
-    obj->state=RADIO_SYNC_OK;
-    obj->sp=0;
-  }
-  else
-  {
-    obj->sp=0;
-    return;
-  }
-}
-
-void radio_rx_get_bit(radioRXC *obj,uint8_t bit_value)
-{
-  if(obj->index<66)
-  {
-    if(obj->index<32)
-    {
-      obj->dataE=obj->dataE<<1;
-      if(bit_value)obj->dataE=obj->dataE|1;
+void radio_rx_get_bit(uint8_t bit_value) {
+  if (radio.index < 66) {
+    if (radio.index < 32) {
+      radio.dataE = radio.dataE << 1;
+      if (bit_value) radio.dataE = radio.dataE | 1;
+    } else if (radio.index < 64) {
+      radio.dataF = radio.dataF << 1;
+      if (bit_value) radio.dataF = radio.dataF | 1;
+    } else if (radio.index < 66) {
+      radio.dataVR = radio.dataVR << 1;
+      if (bit_value) radio.dataVR = radio.dataVR | 1;
     }
-    else if(obj->index<64)
-    {
-      obj->dataF=obj->dataF<<1;
-      if(bit_value)obj->dataF=obj->dataF|1;
-    }
-    else if(obj->index<66)
-    {
-      obj->dataVR=obj->dataVR<<1;
-      if(bit_value)obj->dataVR=obj->dataVR|1;
-    }
-    obj->index++;
-    if(obj->index==66)
-    {
-      obj->state=RADIO_DATA_OK;
+    radio.index++;
+    if (radio.index == 66) {
+      radio.state = RADIO_DATA_OK;
     }
   }
 }
 
-void radio_rx_get_err(radioRXC *obj)
-{
-  obj->state=RADIO_LISTEN;
-  obj->sp=0;
+void radio_rx_get_err(void) {
+  radio.state = RADIO_LISTEN;
+  radio.sp = 0;
 }
 
-void radio_ckeck_bit_pulse(radioRXC *obj,oneBit *d)
-{
-  if(d->on<d->off)
-  {
-    if((d->on*3)>d->off)
-    {
-      radio_rx_get_bit(obj,1);
-      if(!obj->pulselength)obj->pulselength=d->on;
-    }
-    else radio_rx_get_err(obj);
-  }
-  else
-  {
-    if((d->off*3)>d->on)radio_rx_get_bit(obj,0);
-    else radio_rx_get_err(obj);
+void radio_ckeck_bit_pulse(void) {
+  if (tempb.on < tempb.off) {
+    if ((tempb.on * 3) > tempb.off) {
+      radio_rx_get_bit(1);
+      if (!radio.pulselength) radio.pulselength = tempb.on;
+    } else radio_rx_get_err();
+  } else {
+    if ((tempb.off * 3) > tempb.on) radio_rx_get_bit(0);
+    else radio_rx_get_err();
   }
 }
 
-void radio_check_sig_pulse(radioRXC *obj,oneBit *d) {
-  if(radio_is_valid_pulse(obj, d))
-  {
-    switch(obj->state)
-    {
-      case RADIO_OFF:break;
-      case RADIO_LISTEN:radio_check_synce_pulse(obj,d);break;
-      case RADIO_SYNC_OK:radio_ckeck_bit_pulse(obj,d);break;
-      case RADIO_DATA_OK:break;
-      default:break;
+void radio_check_sig_pulse(void) {
+  uint16_t m = min(tempb.on, tempb.off);
+  if (m > 300 && m < 500) {
+    switch (radio.state) {
+      case RADIO_OFF: break;
+      case RADIO_LISTEN: radio_check_synce_pulse(); break;
+      case RADIO_SYNC_OK: radio_ckeck_bit_pulse(); break;
+      case RADIO_DATA_OK: break;
+      default: break;
     }
   }
 }
 
-void radio_rx_reset(radioRXC *obj)
-{
-  obj->state=RADIO_LISTEN;
+void radio_rx_reset(void) {
+  radio.state = RADIO_LISTEN;
 }
 
-void radio_rx_stop(radioRXC *obj)
-{
-  obj->state=RADIO_OFF;
+void radio_rx_stop(void) {
+  radio.state = RADIO_OFF;
 }
 
-bool radio_rx_data_is_ready(radioRXC *obj)
-{
-  if(radio.state==RADIO_DATA_OK) return true;
+bool radio_rx_data_is_ready(void) {
+  if (radio.state == RADIO_DATA_OK) return true;
   else return false;
 }
 
 void RADIO_SIGNAL_PIN_handler() {
-  static uint32_t tempt = 0, oldt = 0, dift = 0;
-  static oneBit tempb;
+
 
   tempt = micros();
   dift = tempt - oldt;
   if (digitalRead(RADIO_SIGNAL_PIN)) {
     tempb.off = dift;
-    radio_check_sig_pulse(&radio,&tempb);
+    radio_check_sig_pulse();
   } else {
     tempb.on = dift;
   }
@@ -182,14 +141,13 @@ void setup() {
   Serial.begin(115200);
   Serial.println("\r\nStarting..");
   pinMode(LED_PIN, OUTPUT);
-  radio_init(&radio);
+  radio_init();
   Serial.println("Ready");
 }
 
 void loop() {
-  if(radio_rx_data_is_ready(&radio))
-  {
+  if (radio_rx_data_is_ready()) {
     blink();
-    radio_rx_reset(&radio);
+    radio_rx_reset();
   }
 }
