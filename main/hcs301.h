088498422b175a4690145c12f2499f8458d17f65
Esmaeill <liyanboy74@gmail.com>
2024-02-29 14:48:43 +0330

    add some code

--- main/hcs301.h
+++ main/hcs301.h
@@ -1,13 +1,13 @@
 
 struct hcsEncrypted{
-  uint32_t counter:16;
-  uint32_t disc:10;
-  uint32_t ovr:2;
   uint32_t btn:4;
+  uint32_t ovr:2;
+  uint32_t disc:10;
+  uint32_t counter:16;
 };
 
 struct hcsFixed{
-  uint32_t ser:28;
   uint32_t btn:4;
+  uint32_t ser:28;
 };
 
--- main/main.ino
+++ main/main.ino
@@ -9,6 +9,7 @@
 #define LED_PIN LED_BUILTIN
 
 char Buffer[64];
+uint8_t i=0;
 
 uint64_v key= {0x5cec6701,0xb79fd949};
 
@@ -31,19 +32,21 @@ void setup() {
 
 void loop() {
   if (radio_rx_data_is_ready(&radio)) {
-
+    blink();
     
     hcs_fix=*(struct hcsFixed*)&radio.dataF;
 
-    ciphertext=radio.dataE;
+    ciphertext=(uint32_t)radio.dataE;
     keeloq_decrypt(&key,&plaintext,&ciphertext,KEELOQ_NROUNDS);
     hcs_enc=*(struct hcsEncrypted*)&plaintext;
 
-    sprintf(Buffer, "%x \r\n",hcs_fix.ser);
-    
+    sprintf(Buffer,"%03d: fix=%08lX enc=%08lX ",++i,radio.dataF,radio.dataE);
+    Serial.print(Buffer);
+    sprintf(Buffer,"btn=%lX ser=%lX dec=[%08lX]-> ",hcs_fix.btn,hcs_fix.ser,plaintext);
+    Serial.print(Buffer);
+    sprintf(Buffer,"btn=[%lX] ovr=[%lX] disc=[%lX] C=[%lX] \r\n",hcs_enc.btn,hcs_enc.ovr,hcs_enc.disc,hcs_enc.counter);
     Serial.print(Buffer);
 
-    blink();
     radio_rx_reset(&radio);
   }
 }
